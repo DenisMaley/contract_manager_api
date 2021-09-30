2ca1770270c851faed7cbc8f8e028d77a9372545
dmaley <dmaley@appannie.com>
2021-10-01 00:10:32 +0200

    error handling

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json());
 app.use('/', routes); //to use the routes
 
+// middleware to catch non-existing routes
+app.use((req, res, next) => {
+    res.status(404).json({message: "Path not found."});
+});
+
+// middleware to catch errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    // REST API Best practice: We do want to see the error but we don't want
+    // to let a user see it, so just "Something went wrong"
+    res.status(500).json({error: "Something went wrong"});
+});
+
 const listener = app.listen(process.env.PORT || port, () => {
     console.log('App is listening on port ' + listener.address().port);
 });
